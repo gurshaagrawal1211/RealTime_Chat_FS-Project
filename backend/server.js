fc923fc36257796ba56590e789c1dde10216809c
gurshaagrawal1211 <gurshaagrawal@gmail.com>
2023-03-12 10:43:04 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,24 @@
 const express=require("express");
+const{chats}=require("./data/data");
 const dotenv=require("dotenv");
-// 
+const connectDB=require("./config/db")
+const userRoutes=require("./routes/userRoutes")
 
-const app=express();
+
 dotenv.config();
+connectDB();
+const app=express();
 
-app.get("/",(req,res)=>
-{
-    res.send("API is Running");
-});
+app.use(express.json()) //to accept json data
 
-// app.get("/api/chat",(req,res)=>
-// {
-//     res.send(chats);
-// });
+var cors=require('cors');
 
-// app.get("/api/chat/:id",(req,res)=>
-// {
-//    // console.log(req.params.id);
-//    const singleChat=chats.find((c)=>c._id===req.params.id);
-// });
+ app.get('/',(req,res)=>
+ {
+    res.send("API is running ");
+ });
 
-const PORT=process.env.PORT || 7000
 
-app.listen(`${PORT}`, console.log(`Server started on PORT ${PORT}`));
+const PORT=process.env.PORT || 8000;
+
+app.listen(`${PORT}`, console.log(`Server Started on PORT ${PORT}`));
